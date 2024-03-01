508337cc3c995edb7d09474afd39e66eed80cc81
arpittiwari24 <atofficial2410@gmail.com>
2024-03-01 22:46:01 +0530

    added ui changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,39 +4,36 @@ import { signIn, signOut, useSession } from "next-auth/react"
 const Navbar = () => {
   const {data: session} = useSession()
   return (
-    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap  w-full  text-sm py-0 sm:py-2">
-  <nav
-    className="relative max-w-7xl w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-0"
-    aria-label="Global"
-  >
-    <div className="flex items-center justify-between">
-      <a
-        className="flex-none text-3xl font-semibold text-white"
-      >
-        PromoGenie
-      </a>
-    </div>
- {session ? (
-  <button className="p-[3px] relative">
-            <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-            <div className="px-8 py-2  bg-black  rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent"
+   <header className=" max-sm:gap-2 max-sm:flex-row max-sm:items-center max-sm:justify-center w-full text-sm py-0 sm:py-2">
+  <nav className="relative max-w-7xl w-full mx-auto px-4 max-sm:flex max-sm:items-center max-sm:justify-between" aria-label="Global">
+    <div className="flex items-center justify-between max-sm:justify-center max-sm:gap-12">
+      <a className="flex-none text-3xl max-sm:text-xl font-semibold text-white">PromoGenie</a>
+      {session ? (
+        <button className="p-[3px] relative">
+          <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+          <div
+            className="px-8 py-2  bg-black  rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent"
             onClick={() => signOut()}
-            >
-              Sign Out 
-            </div>
-            </button>
- ) : (
-  <button className="p-[3px] relative">
-            <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-            <div className="px-8 py-2  bg-black  rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent"
+          >
+            Sign Out
+          </div>
+        </button>
+      ) : (
+        <button className="p-[3px] relative">
+          <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+          <div
+            className="px-8 py-2  bg-black  rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent"
             onClick={() => signIn('google')}
-            >
-              Sign up 
-            </div>
-            </button>
- )}
+          >
+            Sign up
+          </div>
+        </button>
+      )}
+    </div>
   </nav>
 </header>
+
+
   )
 }
 
