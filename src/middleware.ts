75cce12b5319620f7cad0ed3843be7098761400d
arpittiwari24 <atofficial2410@gmail.com>
2024-04-05 21:39:50 +0530

    added subsequent routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export {default} from "next-auth/middleware"
 
-export const config = { matcher : ['/dashboard']}
+export const config = { matcher : ['/dashboard','/dashboard/Linkedin_Post','/dashboard/Twitter_Post','/dashboard/Instagram_caption','/dashboard/Cold_Email','/dashboard/Blog_Page','/dashboard/Whatsapp_biz','/dashboard/History']}
